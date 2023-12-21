b367569542a8bf5a2fb0e53dd978e5e22987d928
Alexander Smirnov <furiousteabag@gmail.com>
2023-12-21 14:58:55 +0530

    clean up result estimation calculation

--- app/_lib/index.ts
+++ app/_lib/index.ts
@@ -1,7 +1,131 @@
 import { ModelConfig, Optimizer, Precision, ResultEstimation, RunConfig, Unit } from "@/app/_interfaces"
 
-export function round(num: number, fractionDigits: number): number {
-  return Number(num.toFixed(fractionDigits))
+export function estimateResult({
+  modelConfig,
+  runConfig,
+  unit,
+}: {
+  modelConfig: ModelConfig
+  runConfig: RunConfig
+  unit: Unit
+}): ResultEstimation {
+  /*
+   * Auxilary variables.
+   */
+
+  const { numParams, vocabSize } = modelConfig
+  const {
+    optimizer,
+    optimizerSGDMomentum,
+    batchSize,
+    sequenceLength,
+    numGPUs,
+    isTraining,
+    trainingPrecision,
+    inferencePrecision,
+    isFSDP,
+    isInferenceModelParallelism,
+  } = runConfig
+
+  const bytesPerParam = isTraining
+    ? trainingPrecision == Precision.mixed
+      ? 6
+      : 4
+    : inferencePrecision == Precision.full
+      ? 4
+      : 2
+  const isParallelMode = (!isTraining && isInferenceModelParallelism) || (isTraining && isFSDP)
+  const gpuDivisor = numGPUs > 1 && isParallelMode ? numGPUs : 1
+
+  const divisor = unit == "MiB" ? 2 ** 20 : 2 ** 30
+  const precision = unit == "MiB" ? 0 : 3
+
+  /*
+   * Actual calculations.
+   */
+
+  // When PyTorch uses CUDA for the first time,
+  // it allocates between 300 MiB and 2 GiB of VRAM.
+  const cudaKernels = 1000 * 2 ** 20
+
+  const parameters = (bytesPerParam * numParams * 10 ** 9) / gpuDivisor
+  const activations = calculateActivations({ modelConfig, runConfig })
+
+  // On training storing probabilities after softmax
+  // output which are the same size as output.
+  const outputs = 4 * batchSize * sequenceLength * vocabSize * (isTraining ? 2 : 1)
+
+  const gradients = (4 * numParams * 10 ** 9) / gpuDivisor
+  const firstMoments = (4 * numParams * 10 ** 9) / gpuDivisor
+  const secondMoments = (4 * numParams * 10 ** 9) / gpuDivisor
+
+  const resultEsimation: ResultEstimation = {
+    cudaKernels: round(cudaKernels / divisor, precision),
+    parameters: round(parameters / divisor, precision),
+    outputs: round(outputs / divisor, precision),
+    activations: round(activations / divisor, precision),
+    gradients: isTraining ? round(gradients / divisor, precision) : undefined,
+    firstMoments:
+      isTraining && ((optimizer == Optimizer.SGD && optimizerSGDMomentum) || optimizer == Optimizer.Adam)
+        ? round(firstMoments / divisor, precision)
+        : undefined,
+    secondMoments: isTraining && optimizer == Optimizer.Adam ? round(secondMoments / divisor, precision) : undefined,
+  }
+
+  return resultEsimation
+}
+
+/**
+ * Calculates number of bytes required for activations.
+ * Based on https://arxiv.org/pdf/2205.05198.pdf.
+ *
+ * On training, returns number of bytes across all layers,
+ * whereas on inference return number of bytes required to
+ * store activations across single layer.
+ */
+function calculateActivations({ modelConfig, runConfig }: { modelConfig: ModelConfig; runConfig: RunConfig }): number {
+  const { hiddenSize, numAttentionHeads, numKeyValueHeads, intermediateSize, numLayers } = modelConfig
+  const { batchSize, sequenceLength, numGPUs, isTraining, trainingPrecision, inferencePrecision, isFSDP } = runConfig
+
+  // Activations take 2 bytes in case of training with mixed
+  // precision or inference with half precision.
+  const bytesPerParam =
+    (isTraining && trainingPrecision === Precision.mixed) || (!isTraining && inferencePrecision !== Precision.full)
+      ? 2
+      : 4
+  const numKeyValHeads = numKeyValueHeads
+  const intermSize = intermediateSize
+  const headDim = hiddenSize / numAttentionHeads
+
+  const attentionInput = bytesPerParam * batchSize * sequenceLength * hiddenSize
+  const q = bytesPerParam * batchSize * sequenceLength * headDim * numAttentionHeads
+  const k = bytesPerParam * batchSize * sequenceLength * headDim * numKeyValHeads
+  const softmaxOutput = bytesPerParam * batchSize * numAttentionHeads * Math.pow(sequenceLength, 2)
+  const softmaxDropoutMask = 1 * batchSize * numAttentionHeads * Math.pow(sequenceLength, 2)
+  const dropoutOutput = bytesPerParam * batchSize * numAttentionHeads * Math.pow(sequenceLength, 2)
+  const v = bytesPerParam * batchSize * sequenceLength * headDim * numKeyValHeads
+  const outProjInput = bytesPerParam * batchSize * sequenceLength * numAttentionHeads * headDim
+  const attentionDropout = 1 * batchSize * sequenceLength * hiddenSize
+  const attentionBlock =
+    attentionInput + q + k + softmaxOutput + v + outProjInput + softmaxDropoutMask + dropoutOutput + attentionDropout
+
+  const mlpInput = bytesPerParam * batchSize * sequenceLength * hiddenSize
+  const activationInput = bytesPerParam * batchSize * sequenceLength * intermSize
+  const downProjInput = bytesPerParam * batchSize * sequenceLength * intermSize
+  const dropoutMask = 1 * batchSize * sequenceLength * hiddenSize
+  const mlpBlock = mlpInput + activationInput + downProjInput + dropoutMask
+
+  const layerNorms = bytesPerParam * batchSize * sequenceLength * hiddenSize * 2
+
+  const layer = attentionBlock + mlpBlock + layerNorms
+
+  let activations = isTraining ? layer * numLayers : layer
+
+  if (isTraining && numGPUs > 1 && isFSDP) {
+    activations /= numGPUs
+  }
+
+  return activations
 }
 
 export function getTotalUsagePerGPU({
@@ -26,113 +150,6 @@ export function getTotalUsagePerGPU({
   )
 }
 
-function calculateActivations({ modelConfig, runConfig }: { modelConfig: ModelConfig; runConfig: RunConfig }): number {
-  const bytesPerParam = runConfig.isTraining
-    ? runConfig.trainingPrecision == Precision.mixed
-      ? 2
-      : 4
-    : runConfig.inferencePrecision == Precision.full
-      ? 4
-      : 2
-
-  const { hiddenSize, numAttentionHeads, numKeyValueHeads, intermediateSize, numLayers } = modelConfig
-  const numKeyValHeads = numKeyValueHeads
-  const intermSize = intermediateSize
-  const headDim = hiddenSize / numAttentionHeads
-
-  const { batchSize: bs, sequenceLength: seqLength } = runConfig
-
-  const attentionInput = bytesPerParam * bs * seqLength * hiddenSize
-  const q = bytesPerParam * bs * seqLength * headDim * numAttentionHeads
-  const k = bytesPerParam * bs * seqLength * headDim * numKeyValHeads
-  const softmaxOutput = bytesPerParam * bs * numAttentionHeads * Math.pow(seqLength, 2)
-  const softmaxDropoutMask = 1 * bs * numAttentionHeads * Math.pow(seqLength, 2)
-  const dropoutOutput = bytesPerParam * bs * numAttentionHeads * Math.pow(seqLength, 2)
-  const v = bytesPerParam * bs * seqLength * headDim * numKeyValHeads
-  const outProjInput = bytesPerParam * bs * seqLength * numAttentionHeads * headDim
-  const attentionDropout = 1 * bs * seqLength * hiddenSize
-  const attentionBlock =
-    attentionInput + q + k + softmaxOutput + v + outProjInput + softmaxDropoutMask + dropoutOutput + attentionDropout
-
-  const mlpInput = bytesPerParam * bs * seqLength * hiddenSize
-  const activationInput = bytesPerParam * bs * seqLength * intermSize
-  const downProjInput = bytesPerParam * bs * seqLength * intermSize
-  const dropoutMask = 1 * bs * seqLength * hiddenSize
-  const mlpBlock = mlpInput + activationInput + downProjInput + dropoutMask
-
-  const layerNorms = bytesPerParam * bs * seqLength * hiddenSize * 2
-
-  const layer = attentionBlock + mlpBlock + layerNorms
-
-  return runConfig.isTraining ? layer * numLayers : layer
-}
-
-export function estimateResult({
-  modelConfig,
-  runConfig,
-  unit,
-}: {
-  modelConfig: ModelConfig
-  runConfig: RunConfig
-  unit: Unit
-}): ResultEstimation {
-  const divisor = unit == "MiB" ? 2 ** 20 : 2 ** 30
-  const precision = unit == "MiB" ? 0 : 3
-
-  const bytesPerParam = runConfig.isTraining
-    ? runConfig.trainingPrecision == Precision.mixed
-      ? 6
-      : 4
-    : runConfig.inferencePrecision == Precision.full
-      ? 4
-      : 2
-
-  // const activations =
-  //   (runConfig.isTraining
-  //     ? runConfig.trainingPrecision == Precision.mixed
-  //       ? 1 * modelConfig.numLayers
-  //       : 2 * modelConfig.numLayers
-  //     : runConfig.inferencePrecision == Precision.full
-  //       ? 2
-  //       : 1) *
-  //   runConfig.sequenceLength *
-  //   runConfig.batchSize *
-  //   modelConfig.hiddenSize *
-  //   (34 + (5 * modelConfig.numAttentionHeads * runConfig.sequenceLength) / modelConfig.hiddenSize)
-  // const outBytesPerParam = Math.max(modelConfig.outPrecision == Precision.full ? 4 : 2, bytesPerParam)
-
-  let activations = calculateActivations({ modelConfig, runConfig })
-
-  if (runConfig.numGPUs > 1 && runConfig.isTraining && runConfig.isFSDP) {
-    activations /= runConfig.numGPUs
-  }
-
-  const gpuDivisor =
-    runConfig.numGPUs > 1 &&
-    ((!runConfig.isTraining && runConfig.isInferenceModelParallelism) || (runConfig.isTraining && runConfig.isFSDP))
-      ? runConfig.numGPUs
-      : 1
-
-  const resultEsimation: ResultEstimation = {
-    cudaKernels: round((1000 * 2 ** 20) / divisor, precision),
-    parameters: round((bytesPerParam * modelConfig.numParams * 10 ** 9) / gpuDivisor / divisor, precision),
-    outputs: round(
-      ((runConfig.isTraining ? 2 : 1) * 4 * runConfig.batchSize * runConfig.sequenceLength * modelConfig.vocabSize) /
-        divisor,
-      precision,
-    ),
-    activations: round(activations / divisor, precision),
-  }
-
-  if (runConfig.isTraining) {
-    resultEsimation.gradients = round((4 * modelConfig.numParams * 10 ** 9) / gpuDivisor / divisor, precision)
-    if (runConfig.optimizer == Optimizer.SGD && runConfig.optimizerSGDMomentum) {
-      resultEsimation.firstMoments = round((4 * modelConfig.numParams * 10 ** 9) / gpuDivisor / divisor, precision)
-    }
-    if (runConfig.optimizer == Optimizer.Adam) {
-      resultEsimation.firstMoments = round((4 * modelConfig.numParams * 10 ** 9) / gpuDivisor / divisor, precision)
-      resultEsimation.secondMoments = round((4 * modelConfig.numParams * 10 ** 9) / gpuDivisor / divisor, precision)
-    }
-  }
-  return resultEsimation
+export function round(num: number, fractionDigits: number): number {
+  return Number(num.toFixed(fractionDigits))
 }
